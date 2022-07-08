cc84e45ac9523764b5e4165913c727061a227d7e
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2022-07-08 09:33:51 +0100

    f

--- Front-End/my-app/src/Funuctions/DBconnects/Anaylitics.js
+++ Front-End/my-app/src/Funuctions/DBconnects/Anaylitics.js
@@ -1,6 +1,10 @@
 //Server configuration
 
 var AnalyticsLink = "https://analyticsbtcprice.herokuapp.com";
+<<<<<<< HEAD
 //var AnalyticsLink = "http://localhost:8081";
+=======
+// var AnalyticsLink = "http://localhost:8081";
+>>>>>>> parent of a3248a4 (Reverted back to dev back,)
 
 export default AnalyticsLink;
