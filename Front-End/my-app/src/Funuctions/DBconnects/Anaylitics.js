d8ed3b5de9fe055a97c5f6c4c649737c747f3d8d
ClintonProjects <77628353+ClintonProjects@users.noreply.github.com>
2022-07-11 14:30:01 +0100

    f

--- Front-End/my-app/src/Funuctions/DBconnects/Anaylitics.js
+++ Front-End/my-app/src/Funuctions/DBconnects/Anaylitics.js
@@ -1,6 +1,6 @@
 //Server configuration
 
-// var AnalyticsLink = "https://analyticsbtcprice.herokuapp.com";
-var AnalyticsLink = "http://localhost:8081";
+var AnalyticsLink = "https://analyticsbtcprice.herokuapp.com";
+// var AnalyticsLink = "http://localhost:8081";
 
 export default AnalyticsLink;
